f3a1d994ee31f9d03f561c8398db8c1b1dabe251
Swann HERERERA <swann.herrera@gmail.com>
2020-02-17 22:38:22 +0100

    jwt mise en place

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -25,8 +25,9 @@
       </p>
       <p class="text-center mb-4">
         <button
+          type="button"
           class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-          type="submit"
+          @click="postLogin"
         >
           Sign In
         </button>
@@ -36,10 +37,27 @@
 </template>
 <script>
 import myInput from '@/components/myInput.vue'
+
+const Cookie = process.client ? require('js-cookie') : undefined
+
 export default {
+  middleware: 'notAuthenticated',
   layout: 'auth',
   components: {
     myInput
+  },
+  methods: {
+    postLogin() {
+      setTimeout(() => {
+        // we simulate the async request with timeout.
+        const auth = {
+          accessToken: 'someStringGotFromApiServiceWithAjax'
+        }
+        this.$store.commit('setAuth', auth) // mutating to store for client rendering
+        Cookie.set('auth', auth) // saving token in cookie for server rendering
+        this.$router.push('/admin')
+      }, 1000)
+    }
   }
 }
 </script>
