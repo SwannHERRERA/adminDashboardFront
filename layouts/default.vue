8a11bfa65f0299053b816f48b8ba4e5b5d7310c0
Swann HERERERA <swann.herrera@gmail.com>
2020-03-01 18:23:50 +0100

    clear all

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="bg-indigo-100 w-full flex flex-row flex-wrap h-screen">
-    <sidebar />
     <nuxt />
   </div>
 </template>
 <script>
-import sidebar from '@/components/sidebar.vue'
-export default {
-  components: {
-    sidebar
-  }
-}
+export default {}
 </script>
--- pages/test.vue
+++ pages/test.vue
@@ -1,17 +1,5 @@
 <template>
-  <div>
-    <dl-input id="test" design="underline" placeholder="placeholder" />
-    <dl-button id="" color="teal">Test button</dl-button>
-  </div>
+  <div></div>
 </template>
 
-<script>
-import dlInput from '@/components/atoms/dlInput.vue'
-import dlButton from '@/components/atoms/dlButton.vue'
-export default {
-  components: {
-    'dl-input': dlInput,
-    'dl-button': dlButton
-  }
-}
-</script>
+<script></script>
