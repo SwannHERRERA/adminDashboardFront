3ee9f58f6532e20c9bcfb6f33ffd5f25027f9b03
Swann HERERERA <swann.herrera@gmail.com>
2020-03-01 18:18:46 +0100

    create discution

--- components/molecules/formGroup.vue
+++ components/molecules/formGroup.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <dl-input id="test" placeholder="placeholder" />
+    <label class="block text-gray-700 text-sm font-bold mb-2" :for="id"
+      ><slot></slot
+    ></label>
+    <dl-input :id="id" :type="type" :value="value" :placeholder="placeholder" />
   </div>
 </template>
 
@@ -9,6 +12,37 @@ import dlInput from '@/components/atoms/dlInput.vue'
 export default {
   components: {
     'dl-input': dlInput
+  },
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    },
+    name: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    value: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    design: {
+      type: String,
+      required: false,
+      default: 'classic'
+    }
   }
 }
 </script>
